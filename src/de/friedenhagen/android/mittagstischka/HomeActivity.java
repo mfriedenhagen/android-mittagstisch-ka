f506d80966d822fd6e9dbb0271b1fa35642e34af
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-07-19 23:23:46 +0200

    Button

--- src/de/friedenhagen/android/mittagstischka/HomeActivity.java
+++ src/de/friedenhagen/android/mittagstischka/HomeActivity.java
@@ -2,8 +2,6 @@ package de.friedenhagen.android.mittagstischka;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 public class HomeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -12,14 +10,9 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         final MittagsTischRetriever retriever = new MittagsTischRetriever();
-        final ScrollView scrollView = new ScrollView(this);
-        final TextView textView = new TextView(this);
-        textView.setText("Mittagstisch Karlsruhe");
-        final TextView textView2 = new TextView(this);
-        textView2.setText("Liste");
-        scrollView.addView(textView);
-        scrollView.addView(textView2);
-        setContentView(scrollView);
+//        final TextView t1 = (TextView) findViewById(R.id.textView1);
+//        final TextView t2 = (TextView) findViewById(R.id.textView2);
+//        final ListView listView = (ListView) findViewById(R.id.listView);
 //        try {
 //            retriever.retrieve();
 //        } catch (ClientProtocolException e) {
@@ -27,5 +20,9 @@ public class HomeActivity extends Activity {
 //        } catch (IOException e) {
 //            throw new RuntimeException("Message:", e);
 //        }
+    }
+    
+    public void quit() {
+        super.finish();
     }
 }
