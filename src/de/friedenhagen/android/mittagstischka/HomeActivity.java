74046f961633ffb1ce38fe2c2753cf8d84c6f28b
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-07-20 22:24:54 +0200

    Now able to retrieve via HTTP.

--- src/de/friedenhagen/android/mittagstischka/HomeActivity.java
+++ src/de/friedenhagen/android/mittagstischka/HomeActivity.java
@@ -1,28 +1,86 @@
 package de.friedenhagen.android.mittagstischka;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import de.friedenhagen.android.mittagstischka.MittagsTischRetriever.ApiException;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements AnimationListener {
+    private Animation slideIn;
+
+    private Animation slideOut;
+
+    private ProgressBar progressBar;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        slideIn = AnimationUtils.loadAnimation(this, R.anim.slide_in);
+        slideOut = AnimationUtils.loadAnimation(this, R.anim.slide_out);
+        slideIn.setAnimationListener(this);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
         final MittagsTischRetriever retriever = new MittagsTischRetriever();
-//        final TextView t1 = (TextView) findViewById(R.id.textView1);
-//        final TextView t2 = (TextView) findViewById(R.id.textView2);
-//        final ListView listView = (ListView) findViewById(R.id.listView);
-//        try {
-//            retriever.retrieve();
-//        } catch (ClientProtocolException e) {
-//            throw new RuntimeException("Message:", e);
-//        } catch (IOException e) {
-//            throw new RuntimeException("Message:", e);
-//        }
+        final JSONArray response;
+        try {
+            response = retriever.retrieveLocals();
+        } catch (ApiException e) {
+            throw new RuntimeException("Message:", e);
+        }
+        final int length = response.length();
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            try {
+                JSONObject local = response.getJSONObject(i);
+                sb.append(local.getString("title")).append("\n");
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                throw new RuntimeException("Message:", e);
+            }
+        }
+        final TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setText(sb.toString());
+        // final TextView t1 = (TextView) findViewById(R.id.textView1);
+        // final TextView t2 = (TextView) findViewById(R.id.textView2);
+        // final ListView listView = (ListView) findViewById(R.id.listView);
+        // try {
+        // retriever.retrieve();
+        // } catch (ClientProtocolException e) {
+        // throw new RuntimeException("Message:", e);
+        // } catch (IOException e) {
+        // throw new RuntimeException("Message:", e);
+        // }
     }
-    
+
     public void quit() {
         super.finish();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Not needed
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // Not needed
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void onAnimationStart(Animation animation) {
+        progressBar.setVisibility(View.VISIBLE);
     }
 }
--- src/de/friedenhagen/android/mittagstischka/MittagsTischRetriever.java
+++ src/de/friedenhagen/android/mittagstischka/MittagsTischRetriever.java
@@ -4,22 +4,54 @@
 
 package de.friedenhagen.android.mittagstischka;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
 
 /**
  * @author mirko
  * 
  */
 public class MittagsTischRetriever {
+    /**
+     * Thrown when there were problems contacting the remote API server, either because of a network error, or the
+     * server returned a bad status code.
+     */
+    public static class ApiException extends Exception {
+        public ApiException(String detailMessage, Throwable throwable) {
+            super(detailMessage, throwable);
+        }
+
+        public ApiException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
+
+    /**
+     * Thrown when there were problems parsing the response to an API call, either because the response was empty, or it
+     * was malformed.
+     */
+    public static class ParseException extends Exception {
+        public ParseException(String detailMessage, Throwable throwable) {
+            super(detailMessage, throwable);
+        }
+    }
 
     public final static URI MITTAGSTISCH_URL;
 
@@ -43,10 +75,39 @@ public class MittagsTischRetriever {
         get = new HttpGet(MITTAGSTISCH_URL);
     }
 
-    public void retrieve() throws ClientProtocolException, IOException {
-        final HttpResponse response = httpClient.execute(get);
-        final StatusLine statusLine = response.getStatusLine();
-        System.out.println("MittagsTischRetriever.main()" + statusLine);
+    public String retrieve() throws ApiException {
+        final HttpResponse response;
+        try {
+            response = httpClient.execute(get);
+            final StatusLine statusLine = response.getStatusLine();
+            if (statusLine.getStatusCode() != HttpStatus.SC_OK) {
+                throw new ApiException("Status-Line:" + statusLine);
+            }
+            Log.i("MittagsTischRetriever.retrieve()", String.valueOf(statusLine));
+            final HttpEntity entity = response.getEntity();
+            final byte[] sBuffer = new byte[512];
+            final InputStream inputStream = entity.getContent();
+            final ByteArrayOutputStream content = new ByteArrayOutputStream();
+            // Read response into a buffered stream
+            int readBytes = 0;
+            while ((readBytes = inputStream.read(sBuffer)) != -1) {
+                content.write(sBuffer, 0, readBytes);
+            }
+            // Return result from buffered stream
+            return new String(content.toByteArray(), "UTF-8");
+        } catch (ClientProtocolException e) {
+            throw new ApiException("Message:", e);
+        } catch (IOException e) {
+            throw new ApiException("Message:", e);
+        }
     }
-
+    public JSONArray retrieveLocals() throws ApiException {
+        final String response = retrieve();
+        try {
+            return new JSONArray(response);
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            throw new ApiException("Could not parse " + response, e);
+        }
+    }
 }
