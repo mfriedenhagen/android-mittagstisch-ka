5b99e3b8f672e0050af55fe2ee34490285317421
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-07-21 00:10:47 +0200

    Hurray, it is working somehow

---
+++ src/de/friedenhagen/android/mittagstischka/EateryActivity.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2010 Mirko Friedenhagen 
+ */
+
+package de.friedenhagen.android.mittagstischka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author mirko
+ *
+ */
+public class EateryActivity extends Activity {
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
