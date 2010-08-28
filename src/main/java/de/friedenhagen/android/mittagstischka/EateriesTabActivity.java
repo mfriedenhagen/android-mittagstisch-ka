c3a7426d8c7c98a43d7f592fd285e0fc952cf5ff
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-08-28 05:38:23 +0200

    Add Icons

--- src/main/java/de/friedenhagen/android/mittagstischka/EateriesTabActivity.java
+++ src/main/java/de/friedenhagen/android/mittagstischka/EateriesTabActivity.java
@@ -7,16 +7,21 @@ package de.friedenhagen.android.mittagstischka;
 import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 /**
  * @author mirko
- *
+ * 
  */
 public class EateriesTabActivity extends TabActivity {
-    
+
+    private final static String TAG = Constants.LOG_PREFIX + EateriesTabActivity.class.getSimpleName();
+
     private TabHost tabHost;
 
     /** {@inheritDoc} */
@@ -25,20 +30,28 @@ public class EateriesTabActivity extends TabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         tabHost = getTabHost();
-        addSpec("eateries_by_name", "A-Z", EateriesByNameActivity.class);
-        addSpec("eateries_by_date", "Datum", EateriesByDateActivity.class);
-//        addSpec("eateries_by_distance", "Entfernung", EateriesByDistanceActivity.class);
+        addSpec("eateries_by_name", "A-Z", android.R.drawable.ic_menu_sort_alphabetically, EateriesByNameActivity.class);
+        addSpec("eateries_by_date", "Datum", android.R.drawable.ic_menu_month, EateriesByDateActivity.class);
+        // addSpec("eateries_by_distance", "Entfernung", EateriesByDistanceActivity.class);
         tabHost.setCurrentTabByTag("eateries_by_name");
     }
 
     /**
      * @param tabId
+     * @param tabName
+     * @param drawableId
      * @param tabClass
      */
-    void addSpec(final String tabId, final String tabName, final Class<? extends Activity> tabClass) {
-        final Intent intent = new Intent(this, tabClass);        
+    private void addSpec(final String tabId, final String tabName, final int drawableId,
+            final Class<? extends Activity> tabClass) {
+        Log.d(TAG, "Add " + tabId);
+        final Intent intent = new Intent(this, tabClass);
         final TabSpec spec = tabHost.newTabSpec(tabId);
-        spec.setIndicator(tabName);
+        final TextView textView = new TextView(this);
+        final Drawable drawable = getResources().getDrawable(drawableId);
+
+        textView.setText(tabName);
+        spec.setIndicator(tabName, drawable);
         spec.setContent(intent);
         tabHost.addTab(spec);
     }
