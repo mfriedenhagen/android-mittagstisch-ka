8d6b357e683dd2df4b284af9f38ebd19adde8798
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-09-05 00:06:30 +0200

    Add menu, use strings

--- src/main/java/de/friedenhagen/android/mittagstischka/EateriesTabActivity.java
+++ src/main/java/de/friedenhagen/android/mittagstischka/EateriesTabActivity.java
@@ -10,9 +10,10 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
-import android.widget.TextView;
 
 /**
  * @author mirko
@@ -30,27 +31,33 @@ public class EateriesTabActivity extends TabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         tabHost = getTabHost();
-        addSpec("eateries_by_name", "A-Z", android.R.drawable.ic_menu_sort_alphabetically, EateriesByNameActivity.class);
-        addSpec("eateries_by_date", "Datum", android.R.drawable.ic_menu_month, EateriesByDateActivity.class);
+        addSpec("eateries_by_name", R.string.tab_alphabetically_title, android.R.drawable.ic_menu_sort_alphabetically, EateriesByNameActivity.class);
+        addSpec("eateries_by_date", R.string.tab_bydate_title, android.R.drawable.ic_menu_month, EateriesByDateActivity.class);
         // addSpec("eateries_by_distance", "Entfernung", EateriesByDistanceActivity.class);
         tabHost.setCurrentTabByTag("eateries_by_name");
     }
 
+    /** {@inheritDoc} */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
     /**
      * @param tabId
      * @param tabName
      * @param drawableId
      * @param tabClass
      */
-    private void addSpec(final String tabId, final String tabName, final int drawableId,
+    private void addSpec(final String tabId, final int tabNameId, final int drawableId,
             final Class<? extends Activity> tabClass) {
         Log.d(TAG, "Add " + tabId);
+        final String tabName = getResources().getString(tabNameId);
         final Intent intent = new Intent(this, tabClass);
         final TabSpec spec = tabHost.newTabSpec(tabId);
-        final TextView textView = new TextView(this);
         final Drawable drawable = getResources().getDrawable(drawableId);
-
-        textView.setText(tabName);
         spec.setIndicator(tabName, drawable);
         spec.setContent(intent);
         tabHost.addTab(spec);
