3d14e692d5690ac7db21354a10a3a20a3ac438e7
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-09-05 02:22:02 +0200

    Use normal string file instead of asset

--- src/main/java/de/friedenhagen/android/mittagstischka/InfoActivity.java
+++ src/main/java/de/friedenhagen/android/mittagstischka/InfoActivity.java
@@ -4,50 +4,32 @@
 
 package de.friedenhagen.android.mittagstischka;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
-import de.friedenhagen.android.mittagstischka.retrievers.IOUtils;
 
 /**
  * @author mirko
- *
+ * 
  */
 public class InfoActivity extends Activity {
-    
+
     private final static String TAG = Constants.LOG_PREFIX + InfoActivity.class.getSimpleName();
-    
-    private TextView titleView;
-    
+
     private TextView messageView;
 
     /** {@inheritDoc} */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.info);
-        titleView = (TextView) findViewById(android.R.id.text1);
-        messageView = (TextView) findViewById(android.R.id.text2);
-        final String gitHash;
-        try {
-            final InputStream stream = getAssets().open("GITHASH");
-            try {
-            gitHash = IOUtils.toUtf8String(IOUtils.toByteArray(stream));
-            } finally {
-                stream.close();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Message:", e);
-        }
+        setContentView(R.layout.info);        
+        messageView = (TextView) findViewById(android.R.id.text1);
+        final String gitHash = getResources().getString(R.string.info_githash);
         final String message = getResources().getString(R.string.info_message, gitHash, gitHash);
         Log.d(TAG, "raw_message=" + message);        
-        titleView.setText(R.string.info_title);
         messageView.setText(Html.fromHtml(message));
         messageView.setMovementMethod(LinkMovementMethod.getInstance());
     }
