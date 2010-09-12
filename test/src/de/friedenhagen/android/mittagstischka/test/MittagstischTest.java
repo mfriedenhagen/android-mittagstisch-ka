b5cba6ed21b546cf9038cfdc3c4ef87378b0525f
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-09-12 21:34:46 +0200

    Add Test-Project.

---
+++ test/src/de/friedenhagen/android/mittagstischka/test/MittagstischTest.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2010 Mirko Friedenhagen 
+ */
+
+package de.friedenhagen.android.mittagstischka.test;
+
+import android.test.ApplicationTestCase;
+import de.friedenhagen.android.mittagstischka.MittagstischApplication;
+
+/**
+ * @author mirko
+ *
+ */
+public class MittagstischTest extends ApplicationTestCase<MittagstischApplication> {
+
+    /**
+     * @param applicationClass
+     */
+    public MittagstischTest(Class applicationClass) {
+        super(applicationClass);
+        // TODO Auto-generated constructor stub
+    }
+
+}
