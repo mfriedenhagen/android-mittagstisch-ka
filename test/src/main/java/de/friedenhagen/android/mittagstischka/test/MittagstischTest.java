4c0f49106ec7067f1d719d10db9c5b38eb7d9554
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-09-13 21:55:59 +0200

    Add instrumentation

--- test/src/main/java/de/friedenhagen/android/mittagstischka/test/MittagstischTest.java
+++ test/src/main/java/de/friedenhagen/android/mittagstischka/test/MittagstischTest.java
@@ -4,20 +4,40 @@
 
 package de.friedenhagen.android.mittagstischka.test;
 
-import android.test.ApplicationTestCase;
-import de.friedenhagen.android.mittagstischka.MittagstischApplication;
+import android.test.ActivityInstrumentationTestCase2;
+import de.friedenhagen.android.mittagstischka.EateriesTabActivity;
+import de.friedenhagen.android.mittagstischka.R;
 
 /**
  * @author mirko
  *
  */
-public class MittagstischTest extends ApplicationTestCase<MittagstischApplication> {
+public class MittagstischTest extends ActivityInstrumentationTestCase2<EateriesTabActivity> {
+
+    private EateriesTabActivity tabActivity;
 
     /**
-     * @param applicationClass
+     * @param pkg
+     * @param activityClass
      */
-    public MittagstischTest(Class<MittagstischApplication> applicationClass) {
-        super(applicationClass);
+    public MittagstischTest(String pkg, Class<EateriesTabActivity> activityClass) {
+        super(activityClass.getPackage().getName(), activityClass);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        tabActivity = this.getActivity();
+    }
+    
+    public void testPreconditions() {
+        assertNotNull(tabActivity);
+    }
+    
+    public void testTitle() {
+        final String title = tabActivity.getString(R.string.app_name);
+        assertEquals(title, tabActivity.getTitle());
     }
 
 }
