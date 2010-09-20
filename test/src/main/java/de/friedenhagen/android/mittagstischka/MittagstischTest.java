7aff5aec996743060ed99a856714db66d927f824
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-09-20 22:07:48 +0200

    Press tabs

--- test/src/main/java/de/friedenhagen/android/mittagstischka/MittagstischTest.java
+++ test/src/main/java/de/friedenhagen/android/mittagstischka/MittagstischTest.java
@@ -4,27 +4,36 @@
 
 package de.friedenhagen.android.mittagstischka;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 import com.jayway.android.robotium.solo.Solo;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.util.Log;
+import android.view.View;
 import de.friedenhagen.android.mittagstischka.EateriesTabActivity;
 import de.friedenhagen.android.mittagstischka.R;
 
 /**
  * @author mirko
- *
+ * 
  */
 public class MittagstischTest extends ActivityInstrumentationTestCase2<EateriesTabActivity> {
 
+    private final static String TAG = Constants.LOG_PREFIX + MittagstischTest.class.getSimpleName();
+
     private EateriesTabActivity tabActivity;
+
     private Solo solo;
 
     /**
      * @param pkg
      * @param activityClass
      */
-    public MittagstischTest(String pkg, Class<EateriesTabActivity> activityClass) {
-        super(activityClass.getPackage().getName(), activityClass);
+    public MittagstischTest() {
+        super(MittagstischTest.class.getPackage().getName(), EateriesTabActivity.class);
     }
 
     /** {@inheritDoc} */
@@ -34,14 +43,39 @@ public class MittagstischTest extends ActivityInstrumentationTestCase2<EateriesT
         tabActivity = this.getActivity();
         solo = new Solo(getInstrumentation(), tabActivity);
     }
-    
+
+    /** {@inheritDoc} */
+    @Override
+    protected void tearDown() throws Exception {
+        try {
+            solo.finalize();
+        } catch (Throwable e) {
+            throw new RuntimeException("Message:", e);
+        }
+        tabActivity.finish();
+        super.tearDown();
+    }
+
+    @MediumTest
     public void testPreconditions() {
         assertNotNull(tabActivity);
     }
-    
+
+    @MediumTest
     public void testTitle() {
         final String title = tabActivity.getString(R.string.app_name);
         assertEquals(title, tabActivity.getTitle());
     }
 
+    @MediumTest
+    public void testClickOnDateTab() throws InterruptedException {
+        final ArrayList<View> touchables = tabActivity.getTabHost().getTouchables();
+        Log.d(TAG, "touchables = " + touchables);
+        Thread.sleep(TimeUnit.MILLISECONDS.convert(1, TimeUnit.SECONDS));
+        for (final View touchable : touchables) {
+            Log.d(TAG, "touchable = " + touchable);
+            solo.clickOnView(touchable);
+            Thread.sleep(TimeUnit.MILLISECONDS.convert(1, TimeUnit.SECONDS));
+        }
+    }
 }
