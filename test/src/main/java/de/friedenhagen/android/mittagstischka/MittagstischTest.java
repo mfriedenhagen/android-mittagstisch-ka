3ab1bd3671f4fa5118f6d3ceca96388ae33c043e
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-09-23 21:36:00 +0200

    Work around timing issues in tests.

--- test/src/main/java/de/friedenhagen/android/mittagstischka/MittagstischTest.java
+++ test/src/main/java/de/friedenhagen/android/mittagstischka/MittagstischTest.java
@@ -5,7 +5,6 @@
 package de.friedenhagen.android.mittagstischka;
 
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.MediumTest;
@@ -40,7 +39,6 @@ public class MittagstischTest extends ActivityInstrumentationTestCase2<EateriesT
         super.setUp();
         tabActivity = this.getActivity();
         solo = new Solo(getInstrumentation(), tabActivity);
-        sleepSeconds(2);
     }
 
     /** {@inheritDoc} */
@@ -54,21 +52,8 @@ public class MittagstischTest extends ActivityInstrumentationTestCase2<EateriesT
         super.tearDown();
     }
 
-    @MediumTest
-    public void testPreconditions() {
-        assertNotNull(tabActivity);
-    }
-
-    @MediumTest
-    public void testTitle() {
-        final String title = tabActivity.getString(R.string.app_name);
-        assertEquals(title, tabActivity.getTitle());
-    }
-
-    @MediumTest
-    public void testInfoDialog() {
+    private void triggerInfoDialog() {
         solo.sendKey(Solo.MENU);
-        sleepOneSecond();
         solo.clickOnText(tabActivity.getString(R.string.menu_info));
         final String needle = "Homepage";
         assertTrue("Needle '" + needle + "' not found!", solo.searchText(needle));
@@ -76,34 +61,23 @@ public class MittagstischTest extends ActivityInstrumentationTestCase2<EateriesT
     }
 
     @MediumTest
-    public void testClickOnDateTab() {
-        solo.waitForActivity(tabActivity.getClass().getSimpleName(), 1000);
-        final ArrayList<View> touchables = tabActivity.getTabHost().getTouchables();
-        Log.d(TAG, "touchables = " + touchables);
-        sleepOneSecond();
-        for (final View touchable : touchables) {
-            Log.d(TAG, "touchable = " + touchable);
-            solo.clickOnView(touchable);
-            sleepOneSecond();
-        }
+    public void testClickOnTabs() {
+        solo.assertCurrentActivity("Expected tab activity", tabActivity.getClass());
+        triggerInfoDialog();
+        solo.goBack();
+        solo.assertCurrentActivity("Expected tab activity", tabActivity.getClass());
+        final String title = tabActivity.getString(R.string.app_name);
+        assertEquals(title, tabActivity.getTitle());
+        clickOnTab(R.string.tab_alphabetically_title);
+        clickOnTab(R.string.tab_bydate_title);
     }
 
     /**
-     * @throws InterruptedException
+     * @param titleId
      */
-    void sleepOneSecond() {
-        sleepSeconds(1);
-    }
-
-    /**
-     * @param i
-     */
-    void sleepSeconds(final int i) {
-        try {
-            Thread.sleep(TimeUnit.MILLISECONDS.convert(i, TimeUnit.SECONDS));
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("Could not sleep!", e);
-        }
+    void clickOnTab(final int titleId) {
+        final String tabTitle = tabActivity.getString(titleId);
+        Log.d(TAG, "clickOnTab()" + tabTitle);
+        solo.clickOnText(tabTitle);
     }
 }
