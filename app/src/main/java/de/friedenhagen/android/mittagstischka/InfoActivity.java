4ba8c69c0787568a7f3040d35e4a6a4033273e5e
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-10-04 22:53:54 +0200

    Include versions from the manifest.

--- app/src/main/java/de/friedenhagen/android/mittagstischka/InfoActivity.java
+++ app/src/main/java/de/friedenhagen/android/mittagstischka/InfoActivity.java
@@ -4,7 +4,11 @@
 
 package de.friedenhagen.android.mittagstischka;
 
-import android.app.Activity;
+import roboguice.activity.GuiceActivity;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
@@ -15,20 +19,33 @@ import android.widget.TextView;
  * @author mirko
  * 
  */
-public class InfoActivity extends Activity {
+public class InfoActivity extends GuiceActivity {
 
     private final static String TAG = Constants.LOG_PREFIX + InfoActivity.class.getSimpleName();
 
+    @InjectView(android.R.id.text1)
     private TextView messageView;
+
+    @InjectResource(R.string.info_githash)
+    private String gitHash;
 
     /** {@inheritDoc} */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
-        messageView = (TextView) findViewById(android.R.id.text1);
-        final String gitHash = getResources().getString(R.string.info_githash);
-        final String message = getResources().getString(R.string.info_message, gitHash, gitHash.substring(0, 8));
+        final PackageInfo info;
+        final String versionName;
+        final int versionCode;
+        try {
+            info = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionName = info.versionName;
+            versionCode = info.versionCode;
+        } catch (NameNotFoundException e) {
+            throw new RuntimeException("Could not find " + getPackageName(), e);
+        }
+        final String message = getResources().getString(R.string.info_message, gitHash, gitHash.substring(0, 8),
+                versionName, versionCode);
         Log.d(TAG, "raw_message=" + message);
         messageView.setText(Html.fromHtml(message));
         messageView.setMovementMethod(LinkMovementMethod.getInstance());
